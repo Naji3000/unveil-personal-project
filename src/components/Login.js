80857c265e913aca6ff19201b99e4c09e28461f9
Naji3000 <najiali3000@gmail.com>
2019-09-25 12:23:45 -0500

    save

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,102 +1,225 @@
+// import React from 'react';
+// import axios from 'axios'
+// import firebase from 'firebase';
+// import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
+// import './styles/login.css'
+// import {updateUser} from '../redux/reducers/userReducer'
+// import {connect} from 'react-redux'
+// import {Link} from "react-router-dom"
+
+
+// firebase.initializeApp({
+//     apiKey: "AIzaSyA01LUtEjSFwS_4toV0NsHBUzpsd4QfNTU",
+//     authDomain: "unveil-62c36.firebaseapp.com"
+// })
+
+
+
+// class Login extends React.Component{
+//     constructor(){
+//         super()
+//         this.state = {
+//             username:'',
+//             password: '',
+//             shouldRedirect: false,
+//             triedToClick: false,
+//             serverErrorMessage: "",
+//             isSignedIn: false
+//         }
+//     }
+//     uiConfig = {
+//         signInFlow: "popup",
+//         signInOptions: [
+//         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+//         firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+//         firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+//         firebase.auth.GithubAuthProvider.PROVIDER_ID,
+//         firebase.auth.EmailAuthProvider.PROVIDER_ID
+//         ],
+//         callbacks: {
+//         signInSuccess: () => false
+//         }
+//     }
+
+//     componentDidMount = () => {
+//         firebase.auth().onAuthStateChanged(user => {
+//         this.setState({ isSignedIn: !user })
+//         console.log("user", user)
+//         })
+//     }
+
+    // handleChange = (e) => {
+    //     this.setState({
+    //         [e.target.name]: e.target.value
+    //     })
+    // }
+
+    // loginClick = () => {
+    //     const {username, password} = this.state
+    //     if(username === "" && password === ""){
+    //         this.setState({triedToClick: true})
+    //     } else {
+    //         axios.post('/auth/login', {username, password})
+    //         .then(response => {
+    //             console.log("fire");
+    //             this.props.updateUser(response.data)
+    //             this.setState({shouldRedirect: true})
+    //         }).catch(err => {
+    //             this.setState({serverErrorMessage: err.response.data.error})
+    //         })
+    //     }
+    // }
+//     render(){
+        
+//         console.log(this.props)
+
+//         return (
+//         )
+//     }
+// }
+
+
+
 import React from 'react';
 import axios from 'axios'
+import firebase from 'firebase';
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
 import './styles/login.css'
 import {updateUser} from '../redux/reducers/userReducer'
 import {connect} from 'react-redux'
 import {Link} from "react-router-dom"
 
-class Login extends React.Component{
+
+firebase.initializeApp({
+    apiKey: "AIzaSyA01LUtEjSFwS_4toV0NsHBUzpsd4QfNTU",
+    authDomain: "unveil-62c36.firebaseapp.com"
+})
+
+class Login extends React.Component {
     constructor(){
-        super()
-        this.state = {
-            username:'',
-            password: '',
-            shouldRedirect: false,
-            triedToClick: false,
-            serverErrorMessage: ""
+                super()
+                this.state = {
+                    username:'',
+                    password: '',
+                    shouldRedirect: false,
+                    triedToClick: false,
+                    serverErrorMessage: "",
+                    isSignedIn: false
+                }
+            }
+    uiConfig = {
+        signInFlow: "popup",
+        signInOptions: [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        
+        ],
+        callbacks: {
+            signInSuccess: () => false
         }
     }
 
-    handleChange = (e) => {
-        this.setState({
-            [e.target.name]: e.target.value
+    componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(user => {
+        this.setState({ isSignedIn: !!user })
+            console.log("user", user)
+    })
+}
+
+handleChange = (e) => {
+    this.setState({
+        [e.target.name]: e.target.value
+    })
+}
+
+loginClick = () => {
+    const {username, password} = this.state
+    if(username === "" && password === ""){
+        this.setState({triedToClick: true})
+    } else {
+        axios.post('/auth/login', {username, password})
+        .then(response => {
+            console.log("fire");
+            this.props.updateUser(response.data)
+            this.setState({shouldRedirect: true})
+        }).catch(err => {
+            this.setState({serverErrorMessage: err.response.data.error})
         })
     }
-
-    loginClick = () => {
-        const {username, password} = this.state
-        if(username === "" && password === ""){
-            this.setState({triedToClick: true})
-        } else {
-            axios.post('/auth/login', {username, password})
-            .then(response => {
-                console.log("fire");
-                this.props.updateUser(response.data)
-                this.setState({shouldRedirect: true})
-            }).catch(err => {
-                this.setState({serverErrorMessage: err.response.data.error})
-            })
-        }
-
-    }
-
-    if
+}
 
 
-    render(){
 
-        console.log(this.props)
-
+    render() {
         return (
-            <main className='container-2'>
-                <div className='box'>
-                    
-                    <div className='login-greeting'>
-                    <h3>Hi There!</h3>
-                    </div>
-
-                    <div className='login-greeting-2'>
-
-                    <h5> We're happy to see you again.</h5>
-
-                    </div>
-
-
-
-                            <div className="input-login">
-                                <input 
-                                placeholder='Username'
-                                name='username'
-                                onChange={this.handleChange}/>
-                            </div>
-                            
-                                        <div>
-                                            <input 
-                                            placeholder='Password'
-                                            name='password'
-                                            type='password'
-                                            onChange={this.handleChange}/>
-
-                                        </div>
-                        
-
-
-                    
-                        <Link className='login-button' to='/user'>
-                        <button onClick={this.loginClick}>Login</button>
-                        </Link>
-
-                        <div className='register-text'>
-                        <h6>Don't have an account yet? <Link to='/'>Register here.</Link></h6>
-
-                        </div>
-                        <h6>Or connect with</h6>
-
-                            <button className='google-button'>Google</button>
+            
+        <main className='container-2'>
+    
+            <div className='box'>
                 
-
+    
+    
+                <div className='login-greeting'>
+                <h3>Hi There!</h3>
                 </div>
-            </main>
+    
+                <div className='login-greeting-2'>
+    
+                <h5> We're happy to see you again.</h5>
+    
+                </div>
+    
+                        <div className="input-login">
+                            <input 
+                            placeholder='Username'
+                            name='username'
+                            onChange={this.handleChange}/>
+                        </div>
+                        
+                                    <div>
+                                        <input 
+                                        placeholder='Password'
+                                        name='password'
+                                        type='password'
+                                        onChange={this.handleChange}/>
+    
+                                    </div>
+                    
+    
+    
+                
+                    <Link className='login-button' to='/user'>
+                    <button onClick={this.loginClick}>Login</button>
+                    </Link>
+    
+                    <div className='register-text'>
+                    <h6>Don't have an account yet? <Link to='/'>Register here.</Link></h6>
+                    </div>
+
+                    <h6>Or connect with</h6>
+
+                    <div className='google-button'>
+
+                            {
+                            this.state.isSignedIn ? (
+                        <span>
+                            <div>Signed In!</div>
+                            <button onClick={() => firebase.auth().signOut()}>Sign out!</button>
+                            <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
+                            
+                        </span>
+                        ) : (
+                        <StyledFirebaseAuth
+                            uiConfig={this.uiConfig}
+                            firebaseAuth={firebase.auth()}
+                        />
+                        )}
+
+                    </div>
+                        
+            
+    
+            </div>
+        </main>
         )
     }
 }
