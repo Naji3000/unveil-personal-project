85ed3cf111f0f7ba0cb5a32f908fbf43873f1d38
Naji3000 <najiali3000@gmail.com>
2019-10-07 12:53:09 -0500

    save

--- src/components/PrivacyPol.js
+++ src/components/PrivacyPol.js
@@ -194,9 +194,14 @@ To respond to legal requests and prevent harm. If we receive a subpoena or other
 <br></br> 
 <br></br>                            
 3. WILL YOUR INFORMATION BE SHARED WITH ANYONE?  
+<br></br> 
+<br></br> 
+<br></br> 
+<br></br> 
 
 In Short:  We only share information with your consent, to comply with laws, to provide you with services, to protect your rights, or to fulfill business obligations.   
-
+<br></br> 
+<br></br> 
 
 We may process or share data based on the following legal basis:
 Consent: We may process your data if you have given us specific consent to use your personal information in a specific purpose. 
@@ -206,99 +211,217 @@ Legitimate Interests: We may process your data when it is reasonably necessary t
 Performance of a Contract: Where we have entered into a contract with you, we may process your personal information to fulfill the terms of our contract. 
 
 Legal Obligations: We may disclose your information where we are legally required to do so in order to comply with applicable law, governmental requests, a judicial proceeding, court order, or legal process, such as in response to a court order or a subpoena (including in response to public authorities to meet national security or law enforcement requirements). 
-
-Vital Interests: We may disclose your information where we believe it is necessary to investigate, prevent, or take action regarding potential violations of our policies, suspected fraud, situations involving potential threats to the safety of any person and illegal activities, or as evidence in litigation in which we are involved.
+<br></br> 
+<br></br> 
+Vital Interests:
+<br></br> 
+<br></br> 
+We may disclose your information where we believe it is necessary to investigate, prevent, or take action regarding potential violations of our policies, suspected fraud, situations involving potential threats to the safety of any person and illegal activities, or as evidence in litigation in which we are involved.
 More specifically, we may need to process your data or share your personal information in the following situations:  
 
-Vendors, Consultants and Other Third-Party Service Providers. We may share your data with third party vendors, service providers, contractors or agents who perform services for us or on our behalf and require access to such information to do that work. Examples include: payment processing, data analysis, email delivery, hosting services, customer service and marketing efforts. We may allow selected third parties to use tracking technology on the Services or Apps, which will enable them to collect data about how you interact with the Services or Apps over time. This information may be used to, among other things, analyze and track data, determine the popularity of certain content and better understand online activity. Unless described in this Policy, we do not share, sell, rent or trade any of your information with third parties for their promotional purposes.   
+Vendors, Consultants and Other Third-Party Service Providers.
+<br></br> 
+<br></br> 
+We may share your data with third party vendors, service providers, contractors or agents who perform services for us or on our behalf and require access to such information to do that work. Examples include: payment processing, data analysis, email delivery, hosting services, customer service and marketing efforts. 
+<br></br> 
+<br></br> 
+We may allow selected third parties to use tracking technology on the Services or Apps, which will enable them to collect data about how you interact with the Services or Apps over time. 
+<br></br> 
+<br></br> 
+This information may be used to, among other things, analyze and track data, determine the popularity of certain content and better understand online activity. Unless described in this Policy, we do not share, sell, rent or trade any of your information with third parties for their promotional purposes.   
 
 Business Transfers. We may share or transfer your information in connection with, or during negotiations of, any merger, sale of company assets, financing, or acquisition of all or a portion of our business to another company.
 
-Third-Party Advertisers. We may use third-party advertising companies to serve ads when you visit the Services or Apps. These companies may use information about your visits to our Website(s) and other websites that are contained in web cookies and other tracking technologies in order to provide advertisements about goods and services of interest to you.         
-
-     
-
+Third-Party Advertisers. We may use third-party advertising companies to serve ads when you visit the Services or Apps.
+These companies may use information about your visits to our Website(s) and other websites that are contained in web cookies and other tracking technologies in order to provide advertisements about goods and services of interest to you.         
+<br></br> 
+<br></br> 
+<br></br> 
+<br></br> 
 4. DO WE USE COOKIES AND OTHER TRACKING TECHNOLOGIES?  
+<br></br> 
+<br></br> 
+<br></br> 
+<br></br> 
 In Short:  We may use cookies and other tracking technologies to collect and store your information.  
 
 We may use cookies and similar tracking technologies (like web beacons and pixels) to access or store information. Specific information about how we use such technologies and how you can refuse certain cookies is set out in our Cookie Policy.         
+<br></br> 
+<br></br> 
+<br></br> 
+<br></br> 
 
 
-
-5. HOW LONG DO WE KEEP YOUR INFORMATION?  
+5. HOW LONG DO WE KEEP YOUR INFORMATION? 
+<br></br> 
+<br></br> 
+<br></br> 
+<br></br>  
 
 In Short:  We keep your information for as long as necessary to fulfill the purposes outlined in this privacy policy unless otherwise required by law.   
 
 We will only keep your personal information for as long as it is necessary for the purposes set out in this privacy policy, unless a longer retention period is required or permitted by law (such as tax, accounting or other legal requirements). No purpose in this policy will require us keeping your personal information for longer than the period of time in which users have an account with us .  
 
 When we have no ongoing legitimate business need to process your personal information, we will either delete or anonymize it, or, if this is not possible (for example, because your personal information has been stored in backup archives), then we will securely store your personal information and isolate it from any further processing until deletion is possible.   
-
-
+<br></br> 
+<br></br> 
+<br></br> 
+<br></br> 
 6. HOW DO WE KEEP YOUR INFORMATION SAFE?   
-
+<br></br> 
+<br></br> 
+<br></br> 
+<br></br> 
 In Short:  We aim to protect your personal information through a system of organizational and technical security measures.  
 
 We have implemented appropriate technical and organizational security measures designed to protect the security of any personal information we process. However, please also remember that we cannot guarantee that the internet itself is 100% secure. Although we will do our best to protect your personal information, transmission of personal information to and from our Services or Apps is at your own risk. You should only access the services within a secure environment.   
-
-
-
+<br></br> 
+<br></br> 
+<br></br> 
+<br></br> 
 7. WHAT ARE YOUR PRIVACY RIGHTS?  
-
+<br></br> 
+<br></br> 
+<br></br> 
+<br></br> 
 In Short:  You may review, change, or terminate your account at any time.    
 
 If you are resident in the European Economic Area and you believe we are unlawfully processing your personal information, you also have the right to complain to your local data protection supervisory authority. You can find their contact details here: http://ec.europa.eu/justice/data-protection/bodies/authorities/index_en.htm  
  
-
+<br></br> 
+<br></br> 
 Account Information  
 
 If you would at any time like to review or change the information in your account or terminate your account, you can:
-
+<br></br> 
+<br></br> 
 ■  Contact us using the contact information provided.
-
+<br></br> 
+<br></br> 
 Upon your request to terminate your account, we will deactivate or delete your account and information from our active databases. However, some information may be retained in our files to prevent fraud, troubleshoot problems, assist with any investigations, enforce our Terms of Use and/or comply with legal requirements.
+<br></br> 
+<br></br> 
+Cookies and similar technologies: Most Web browsers are set to accept cookies by default. If you prefer, you can usually choose to set your browser to remove cookies and to reject cookies. 
+<br></br> 
+<br></br> 
+If you choose to remove cookies or reject cookies, this could affect certain features or services of our Services or Apps. 
+<br></br> 
+<br></br> 
 
-Cookies and similar technologies: Most Web browsers are set to accept cookies by default. If you prefer, you can usually choose to set your browser to remove cookies and to reject cookies. If you choose to remove cookies or reject cookies, this could affect certain features or services of our Services or Apps. To opt-out of interest-based advertising by advertisers on our Services or  Apps visit  http://www.aboutads.info/choices/ .  
+To opt-out of interest-based advertising by advertisers on our Services or  Apps visit  http://www.aboutads.info/choices/ .  
+
+<br></br> 
+<br></br> 
 
 Opting out of email marketing: You can unsubscribe from our marketing email list at any time by clicking on the unsubscribe link in the emails that we send or by contacting us using the details provided below. You will then be removed from the marketing email list – however, we will still need to send you service-related emails that are necessary for the administration and use of your account. To otherwise opt-out, you may:
-
+<br></br> 
+<br></br> 
 ■  Contact us using the contact information provided.  
+<br></br> 
+<br></br> 
+<br></br> 
+<br></br> 
 
-
-8. CONTROLS FOR DO-NOT-TRACK FEATURES  
+8. CONTROLS FOR DO-NOT-TRACK FEATURES 
+<br></br> 
+<br></br> 
+<br></br> 
+<br></br>  
 
 Most web browsers and some mobile operating systems and mobile applications include a Do-Not-Track (“DNT”) feature or setting you can activate to signal your privacy preference not to have data about your online browsing activities monitored and collected. No uniform technology standard for recognizing and implementing DNT signals has been finalized. As such, we do not currently respond to DNT browser signals or any other mechanism that automatically communicates your choice not to be tracked online. If a standard for online tracking is adopted that we must follow in the future, we will inform you about that practice in a revised version of this privacy policy.
-
+<br></br> 
+<br></br> 
+<br></br> 
+<br></br> 
 
 9. DO CALIFORNIA RESIDENTS HAVE SPECIFIC PRIVACY RIGHTS?  
+<br></br> 
+<br></br> 
+<br></br> 
+<br></br> 
 
 In Short:  Yes, if you are a resident of California, you are granted specific rights regarding access to your personal information.   
+<br></br> 
+<br></br> 
 
-California Civil Code Section 1798.83, also known as the “Shine The Light” law, permits our users who are California residents to request and obtain from us, once a year and free of charge, information about categories of personal information (if any) we disclosed to third parties for direct marketing purposes and the names and addresses of all third parties with which we shared personal information in the immediately preceding calendar year. If you are a California resident and would like to make such a request, please submit your request in writing to us using the contact information provided below. 
+California Civil Code Section 1798.83, also known as the “Shine The Light” law, permits our users who are California residents to request and obtain from us, once a year and free of charge, information about categories of personal information (if any) we disclosed to third parties for direct marketing purposes and the names and addresses of all third parties with which we shared personal information in the immediately preceding calendar year. 
+<br></br> 
+<br></br> 
+If you are a California resident and would like to make such a request, please submit your request in writing to us using the contact information provided below. 
+<br></br> 
+<br></br> 
 
-If you are under 18 years of age, reside in California, and have a registered account with the Services or Apps, you have the right to request removal of unwanted data that you publicly post on the Services or Apps. To request removal of such data, please contact us using the contact information provided below, and include the email address associated with your account and a statement that you reside in California. We will make sure the data is not publicly displayed on the Services or Apps, but please be aware that the data may not be completely or comprehensively removed from our systems.
+If you are under 18 years of age, reside in California, and have a registered account with the Services or Apps, you have the right to request removal of unwanted data that you publicly post on the Services or Apps. 
+<br></br> 
+<br></br> 
+To request removal of such data, please contact us using the contact information provided below, and include the email address associated with your account and a statement that you reside in California. We will make sure the data is not publicly displayed on the Services or Apps, but please be aware that the data may not be completely or comprehensively removed from our systems.
+
+<br></br> 
+<br></br> 
+<br></br> 
+<br></br> 
 
 
-
-10. DO WE MAKE UPDATES TO THIS POLICY?  
+10. DO WE MAKE UPDATES TO THIS POLICY? 
+<br></br> 
+<br></br> 
+<br></br> 
+<br></br>  
 
 In Short:  Yes, we will update this policy as necessary to stay compliant with relevant laws.  
+<br></br> 
+<br></br> 
+<br></br> 
+<br></br> 
 
 We may update this privacy policy from time to time. The updated version will be indicated by an updated “Revised” date and the updated version will be effective as soon as it is accessible. If we make material changes to this privacy policy, we may notify you either by prominently posting a notice of such changes or by directly sending you a notification. We encourage you to review this privacy policy frequently to be informed of how we are protecting your information.  
 
-
+<br></br> 
+<br></br> 
+<br></br> 
+<br></br> 
 
 11. HOW CAN YOU CONTACT US ABOUT THIS POLICY?  
+<br></br> 
+<br></br> 
+<br></br> 
+<br></br> 
 
 If you have questions or comments about this policy, you may email us at najiali3000@gmail.com or by post to:
+<br></br> 
+<br></br> 
+<br></br> 
+<br></br> 
 
 unveil.live    
+<br></br> 
+<br></br> 
 1850 Cotillion Dr, UNIT# 1309 
-Dunwoody, GA 30338    
-United States        
+<br></br> 
+<br></br> 
+Dunwoody, GA 30338   
+<br></br> 
+<br></br>  
+United States   
+<br></br> 
+<br></br> 
+<br></br> 
+<br></br>      
 
  
 HOW CAN YOU REVIEW, UPDATE, OR DELETE THE DATA WE COLLECT FROM YOU?
-Based on the laws of some countries, you may have the right to request access to the personal information we collect from you, change that information, or delete it in some circumstances. To request to review, update, or delete your personal information, please submit a request form by clicking here. We will respond to your request within 30 days.</p>
+Based on the laws of some countries, you may have the right to request access to the personal information we collect from you, change that information, or delete it in some circumstances. To request to review, update, or delete your personal information, please submit a request to najiali3000@gmail.com
+<br></br> 
+<br></br> 
+ We will respond to your request within 30 days.
+<br></br> 
+<br></br> 
+<br></br> 
+<br></br>
+</p>
+
+
+
             </div>
             </>
         )
