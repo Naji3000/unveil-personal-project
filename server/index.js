b1309759149016f92c2d7d16fb7a988f7fdbcf2f
Naji3000 <najiali3000@gmail.com>
2019-10-08 10:32:21 -0500

    saved

--- server/index.js
+++ server/index.js
@@ -18,8 +18,8 @@ const app = express()
 
 //middleware
 
-app.use(express.json())
-
+app.use(express.json()) 
+app.use( express.static( `${__dirname}/../build` ) ); 
 app.use(cors())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
